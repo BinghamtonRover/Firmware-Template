31631b2beb68f35b594abffaf0bd7732d9afbeab
Levi Lesches <levilesches@gmail.com>
2024-01-31 15:45:21 -0500

    Updated utils

--- Demo/Demo.ino
+++ Demo/Demo.ino
@@ -1,6 +1,4 @@
-#include <BURT_can.h>
-#include <BURT_proto.h>
-#include <BURT_serial.h>
+#include "src/utils/BURT_utils.h"
 
 #include "src/BURT_methane.h"
 #include "src/science.pb.h"
@@ -11,31 +9,41 @@
 
 #define METHANE_PIN 12
 
+// These are "forward declarations". We need them a few lines down, but they're usually
+// long and complex, so we define them at the bottom of the file. This forward declaration
+// tells C++ that we *will* define them later, but here are their signatures *now*.
+void handleScienceCommand(const uint8_t* data, int length);
+void shutdown();
+
 MethaneSensor methaneSensor(METHANE_PIN);
-BurtSerial serial(handleScienceCommand);
+BurtSerial serial(Device::Device_SCIENCE, handleScienceCommand, shutdown);
+BurtCan<Can3> can(SCIENCE_COMMAND_ID, Device::Device_SCIENCE, handleScienceCommand, shutdown);
+
+void setup() {
+  can.setup();
+  serial.setup();
+}
+
+void loop() {
+  can.update();
+  serial.update();
+  sendData();
+  delay(10);
+}
+
+void shutdown() {
+  Serial.println("Device disconnected!");
+}
 
 void handleScienceCommand(const uint8_t* data, int length) {
   // The 2nd parameter is always MessageName_fields
   auto command = BurtProto::decode<ScienceCommand>(data, length, ScienceCommand_fields);
-  if (command.dig) Serial.println("Digging!");
+  if (command.state == ScienceState_COLLECT_DATA) Serial.println("Digging!");
 }
 
 void sendData() { 
   ScienceData data;
   data.methane = methaneSensor.read(); 
   // The 2nd parameter is always MessageName_fields
-  BurtCan::send(SCIENCE_DATA_ID, ScienceData_fields, &data);
-}
-
-void setup() {
-  BurtCan::setup();
-  BurtCan::registerHandler(SCIENCE_COMMAND_ID, handleScienceCommand);
-  // more handlers here...
-}
-
-void loop() {
-  BurtCan::update();
-  sendData();
-  serial.parseSerial();
-  delay(10);
+  can.send(SCIENCE_DATA_ID, &data, ScienceData_fields);
 }
